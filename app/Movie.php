3a481a46c5df8acd49bdb5b2a8fc88a3689f015b
Cassio Henrique Lima Fernandes <cassiofernandes298@gmail.com>
2021-01-27 16:11:05 -0300

    Relationship between Movies and Reviews

--- app/Movie.php
+++ app/Movie.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
+    protected $fillable = ['name', 'year', 'sinopse', 'duration', 'directors', 'writers', 'stars', 'rating', 'image'];
 
+    public function reviews()
+    {
+        return $this->hasMany(Review::class, 'movie_id');
+    }
 }
--- app/Review.php
+++ app/Review.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $fillable = ['name', 'message', 'rating', 'movie_id'];
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class, 'movie_id');
+    }
 }
