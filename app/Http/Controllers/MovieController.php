74c6eca9fddd11a44006a0694e3cd04a06158bbe
Cassio Henrique Lima Fernandes <cassiofernandes298@gmail.com>
2021-01-27 15:13:22 -0300

    Movies - Model, Migration, Routes and Controller

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return response(json_encode($movies), 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $movie = Movie::find($id);
+
+        return response(json_encode($movie), 200);
+    }
+}
